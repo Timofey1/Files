9c8725e57887ae473702fdafaf1930fae671e3a6
Timofey1 <timtwin2@mail.ru>
2019-10-28 13:36:16 +0300

    commit

--- del.py
+++
@@ -1,4 +0,0 @@
-import requests
-for i in range(1):
-    a = requests.get("https://browser-mining.contest.qctf.ru/shop", {"balance": 10000}).text
-    print(a)
--- ks54/apitel.py
+++
@@ -1,3 +0,0 @@
-import requests
-url = "https://api.telegram.org/bot786322386:AAGLNiW1ISK1zFkZPnp1NqQ5wkKqxihuNu8/getUpdates"
-print(requests.get(url))
